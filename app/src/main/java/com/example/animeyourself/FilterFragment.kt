dd9160b8586a4719ad5a6e870bfeafef1676b982
NihalHorseless <onuraslanfb07@hotmail.com>
2023-04-17 17:17:06 +0300

    Trying it with GPUVideo can't apply the filters tho

--- app/src/main/java/com/example/animeyourself/FilterFragment.kt
+++ app/src/main/java/com/example/animeyourself/FilterFragment.kt
@@ -1,6 +1,8 @@
 package com.example.animeyourself
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,20 +10,30 @@ import android.widget.VideoView
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.daasuu.gpuv.composer.FillMode
+import com.daasuu.gpuv.composer.GPUMp4Composer
+import com.daasuu.gpuv.composer.Rotation
+import com.daasuu.gpuv.egl.filter.GlBilateralFilter
+import com.daasuu.gpuv.egl.filter.GlFilter
 import com.example.animeyourself.databinding.FragmentFilterBinding
 import com.google.android.material.chip.ChipGroup
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.launch
 
 
 class FilterFragment : Fragment() {
 
     private lateinit var binding: FragmentFilterBinding
     private val viewModel: FilterViewModel by viewModels()
+    private val TAG = "FilterFragment"
 
     //Fields
     private lateinit var filteredVid: VideoView
     private lateinit var saveBtn: FloatingActionButton
     private lateinit var filterOptions: ChipGroup
+
+    private lateinit var sourceVideoUri: Uri
 
 
     override fun onCreateView(
@@ -37,20 +49,65 @@ class FilterFragment : Fragment() {
         initializeFields()
 
 
+
     }
 
     private fun initializeFields() {
+
         filteredVid = binding.videoView
         saveBtn = binding.floatingActionButton
         filterOptions = binding.chipGroup
         // Get the URI of the video from the arguments
-        val videoUri = arguments?.getString("videoUri")
-
+        sourceVideoUri = arguments?.getString("videoUri")!!.toUri()
+        // Creating temp file to reference path
+        val contentResolver = requireContext().contentResolver
+        val inputStream = contentResolver.openInputStream(sourceVideoUri)
+        val tempFile = createTempFile("temp", null, requireContext().cacheDir)
+        tempFile.outputStream().use { outputStream ->
+            inputStream?.copyTo(outputStream)
+        }
+        applyFilter(GlBilateralFilter(), tempFile.path)
         // Set the video URI as the data source for the VideoView
-        filteredVid.setVideoURI(videoUri?.toUri())
+        Log.d(TAG,"Before start")
+        filteredVid.setVideoURI(sourceVideoUri)
 
         // Start playing the video
         filteredVid.start()
+        Log.d(TAG,"After start")
+
+
+
+
+    }
+    private fun applyFilter(appliedFilter: GlFilter,filePath : String) {
+        lifecycleScope.launch {
+            GPUMp4Composer(filePath, filePath)
+                .rotation(Rotation.ROTATION_90)
+                .size(540, 960)
+                .fillMode(FillMode.PRESERVE_ASPECT_FIT)
+                .filter(appliedFilter)
+                .listener(object : GPUMp4Composer.Listener {
+                    override fun onProgress(progress: Double) {
+                        Log.d(TAG, "onProgress = $progress")
+                    }
+
+                    override fun onCompleted() {
+                        Log.d(TAG, "onCompleted()")
+//                    Toast.makeText(context, "codec complete path =$filePath.toString()", Toast.LENGTH_SHORT).show()
+
+                    }
+
+                    override fun onCanceled() {
+                        Log.d(TAG, "onCanceled")
+                    }
+
+                    override fun onFailed(exception: Exception?) {
+                        Log.e(TAG, "onFailed()", exception)
+                    }
+                })
+                .start()
+
+        }
     }
 
 }
